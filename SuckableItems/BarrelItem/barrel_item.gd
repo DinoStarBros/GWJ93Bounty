1ec8c57b276894730088a224852ccddc27546bc2
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-05-12 17:47:47 +0800

    faster outward speed for barrel items

--- SuckableItems/BarrelItem/barrel_item.gd
+++ SuckableItems/BarrelItem/barrel_item.gd
@@ -6,7 +6,7 @@ var item_resource : SuckableItemResource
 var velocity : Vector2
 
 const lifetime_after : float = 1
-const outward_speed : float = 1000
+const outward_speed : float = 2000
 
 func _physics_process(delta: float) -> void:
 	global_position += delta * velocity
