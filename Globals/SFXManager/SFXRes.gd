e053aae3b8754516b56f3b42cf28eaaf108d7f60
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-05-12 22:04:54 +0800

    more sfx manager setup

---
+++ Globals/SFXManager/SFXRes.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name SFXResource
+
+@export var audio : AudioStream
--- Globals/SFXManager/sfx_manager.gd
+++ Globals/SFXManager/sfx_manager.gd
@@ -1,2 +1,13 @@
 extends Node
 class_name SFXManagerClass
+
+@export var sfxs : Array[SFXResource]
+
+const sfx_scn : PackedScene = preload("res://Scenes/SFX/sfx.tscn")
+const sfx2d_scn : PackedScene = preload("res://Scenes/SFX2d/sfx_2d.tscn")
+
+func play_sfx() -> void:
+	pass
+
+func play_2d_sfx() -> void:
+	pass
