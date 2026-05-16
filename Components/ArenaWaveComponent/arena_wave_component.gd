313f687b49f403dc4e191205f939ffd87d2477e4
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-05-16 23:18:59 +0800

    Enemy spawning waaaaaves

--- Components/ArenaWaveComponent/arena_wave_component.gd
+++ Components/ArenaWaveComponent/arena_wave_component.gd
@@ -1,10 +1,60 @@
 extends Node2D
 class_name ArenaWaveComponent
 
+@export var arena_spawn_domain : CollisionShape2D
 @export var entities_parent : EntitiesParent
 @export var waves : Array[WaveResource]
 
 const enemy_spawn_intro_scn : PackedScene = preload("res://Scenes/EnemySpawnIntro/enemy_spawn_intro.tscn")
 
+var left_bound : float
+var right_bound : float
+var up_bound : float
+var down_bound : float
+
+@onready var spawn_timer: Timer = %SpawnTimer
+
 func _ready() -> void:
-	pass
+	left_bound = (
+		arena_spawn_domain.global_position.x - 
+		(arena_spawn_domain.shape.size.x / 2)
+	)
+	right_bound = (
+		arena_spawn_domain.global_position.x + 
+		(arena_spawn_domain.shape.size.x / 2)
+	)
+	up_bound = (
+		arena_spawn_domain.global_position.y - 
+		(arena_spawn_domain.shape.size.y / 2)
+	)
+	down_bound = (
+		arena_spawn_domain.global_position.y + 
+		(arena_spawn_domain.shape.size.y / 2)
+	)
+	
+	spawn_timer.start(get_spawn_time(waves[Global.current_wave-1].spawn_interval_range))
+
+func _spawn_enemy() -> void:
+	var enemy_spawn_intro : EnemySpawnIntro = enemy_spawn_intro_scn.instantiate()
+	enemy_spawn_intro.enemy_type = waves[Global.current_wave-1].enemies.pick_random()
+	
+	enemy_spawn_intro.global_position.x = (
+		randf_range(left_bound, right_bound)
+	)
+	enemy_spawn_intro.global_position.y = (
+		randf_range(up_bound, down_bound)
+	)
+	
+	Global.entities_parent.add_child(enemy_spawn_intro)
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_timer.start(get_spawn_time(waves[Global.current_wave-1].spawn_interval_range))
+	
+	for n in get_spawn_amnt(waves[Global.current_wave-1].spawn_amount_range):
+		_spawn_enemy()
+
+func get_spawn_time(si_range: Vector2) -> float:
+	return randf_range(si_range.x, si_range.y)
+
+func get_spawn_amnt(sa_range: Vector2i) -> int:
+	return randi_range(sa_range.x, sa_range.y)
--- Waves/WaveResource.gd
+++ Waves/WaveResource.gd
@@ -3,5 +3,9 @@ class_name WaveResource
 
 @export var enemies : Array[EntitiesParent.EnemyTypes] ## Scenes of the enemies that'll be in that wave
 @export var time : float = 30.0 ## Seconds
-@export var spawn_amount : int = 1 ## How many enemies can spawn in one go
-@export var spawn_interval : float = 0.5 ## The time interval between enemies spawning
+## How many enemies can spawn in one go
+## X is min, Y is max
+@export var spawn_amount_range : Vector2i = Vector2i(1,1) 
+## The time interval between enemies spawning
+## X is min, Y is max
+@export var spawn_interval_range : Vector2 = Vector2(0.5,1)
