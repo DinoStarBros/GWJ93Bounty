f3a7c99a851463d7df06c87d773890869cc065f3
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-05-16 10:12:02 +0800

    settings menu in titleScrreen

--- Screens/TitleScreen/title_screen.gd
+++ Screens/TitleScreen/title_screen.gd
@@ -1,11 +1,17 @@
 extends Control
 class_name TitleScreen
 
+@onready var settings_menu: Settings = %settingsMenu
+
 func _ready() -> void:
 	%play.pressed.connect(_play_pressed)
 	%tutorial.pressed.connect(_tutorial_pressed)
 	%settings.pressed.connect(_settings_pressed)
 	%quit.pressed.connect(_quit_pressed)
+	%close.pressed.connect(_close_pressed)
+	
+	settings_menu.hide()
+	settings_menu._load()
 
 func _play_pressed() -> void:
 	pass
@@ -14,7 +20,13 @@ func _tutorial_pressed() -> void:
 	pass
 
 func _settings_pressed() -> void:
-	pass
+	settings_menu.show()
+	settings_menu._load()
 
 func _quit_pressed() -> void:
 	get_tree().quit()
+
+func _close_pressed() -> void:
+	# Closes settings menu
+	settings_menu._save()
+	settings_menu.hide()
